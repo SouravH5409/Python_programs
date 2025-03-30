2ce7342630a7d9e5ef9ce4c8910abc4b16ddc5bc
SoulSourav <souravh5409@gmail.com>
2025-03-30 14:25:14 +0530

    New changes

--- Matrix/matrixop.py
+++ Matrix/matrixop.py
@@ -5,12 +5,12 @@ B = np.random.randint(0, 21, (3, 3))
 
 addition_result = A + B
 
-multiplication_result = np.dot(A, B)
+multiplication_result = np.dot(A, B)   #use of the numpy library
 
 transpose_result = multiplication_result.T
 
-print("Matrix A:\n", A) #matrix 1
-print("Matrix B:\n", B) #matrix 2
+print("Matrix A:\n", A) #matrix A
+print("Matrix B:\n", B) #matrix B
 print("Matrix Addition (A + B):\n", addition_result)  #gives the sum of matrixes
 print("Matrix Multiplication (A * B):\n", multiplication_result) #provides the result of multiplaication of the matrixes
 print("Transpose of the Product Matrix:\n", transpose_result) #gives the transpose of the matrix
