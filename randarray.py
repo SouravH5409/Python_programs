ac40be1c07e9dab81473e10b4dd15fb6d55cb92b
SoulSourav <souravh5409@gmail.com>
2025-04-03 15:06:06 +0530

    New changes

---
+++ randarray.py
@@ -0,0 +1,5 @@
+import numpy as np
+#It mainly takes teh random values using the random function to create an array
+random_array = np.random.randint(30, 41, 10)
+
+print("Random 1-D Array:", random_array)
